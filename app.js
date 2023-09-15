1d1fbe497d31dee9849994ec6b89acd30907ee5d
drnaylayousuf <naylayousuf.dr1310@gmail.com>
2023-09-15 13:01:14 +0500

    third classtask

---
+++ app.js
@@ -0,0 +1,201 @@
+// JS CLASS WORK CHAPTER 14-16
+
+
+// Array chapter
+
+// Q1
+//  var arr = [];
+//  arr[0] = "Nayla";
+//  arr[1] = "laiba";
+//  arr[2] = "Hiba";
+//  arr[3] = "Bushra";
+//  arr[4] = "Hawa";
+//  document.writeln(arr);
+ 
+
+// Q2 (Done)
+
+// var studentNames = {};
+
+// This will create an empty object that can be used to store student names. If you want to add a new student name to the object, you can do so like this:
+
+// studentNames["student1"] = "John Doe";
+
+
+// Q3
+// var arr1 = ["Aliyan","Zain","Nayla","Laiba"];
+// document.write(arr1);
+
+// Q4
+// var arr2 = [2,3,4,5];
+// document.write(arr2);
+
+
+// Q5
+// var arr3 = [true,false];
+// document.write(arr3);
+ 
+// anotherone /way
+
+// var boolArray = [true,false];
+// console.log(boolArray);
+
+// Q6
+// var arr4 = ["Aliyan",12,"Zain",9,"Nayla","Laiba",10];
+// document.write(arr4);
+
+// Q7
+// var degree = [];
+// document.write("QUALIFICATIONS:  ")
+// degree [0] = " SSC";
+// degree [1] = " HSC";
+// degree [2] = " BCS";
+// degree [3] = " BS";
+// degree [4] = " BCOM";
+// degree [5] = " MS";
+// degree [6] = " M.Phil";
+// degree [7] = " PhD";
+
+// document.write("1)" + degree[0]+ "<br>");
+// document.write("2)" + degree[1]+ "<br>");
+// document.write("3)" + degree[3]+ "<br>");
+// document.write("4)" + degree[4]+ "<br>");
+// document.write("5)" + degree[5]+ "<br>");
+// document.write("6)" + degree[6]+ "<br>");
+// document.write("7)" + degree[7]+ "<br>");
+
+
+
+
+// Q8
+// var student = ["John","Harry","William"];
+// var socre = (320 ,230,480);
+// var totalMarks = 500;
+// var percentage = (socre / totalMarks )*100;
+// console.log("Score of Michael is :"+student[0]+percentage.toFixed(2)+"%"); 
+
+// Q9
+// a.
+
+//  var arr = ["red" , "blue", "green" , "pink"];
+//  var userinput = prompt("enter your colour");
+//  arr.unshift(userinput);
+//  console.log(arr);
+
+// b. Q9
+//  var arr = ["red" , "blue", "green" , "pink"];
+//  var userinput = prompt("enter your colour");
+//  arr.push(userinput);
+//  console.log(arr);
+ 
+// c.
+//  var arr = ["red" , "blue", "green" , "pink"];
+//  var userinput = prompt("enter your colour");
+//  arr.unshift(userinput);
+//  console.log(arr);
+
+// d. (start removing)
+//  var arr = ["red" , "blue", "green" , "pink"];
+//  arr.shift();
+//  console.log(arr);
+
+// e. end removing
+//  var arr = ["red" , "blue", "green" , "pink"];
+//  arr.pop();
+//  console.log(arr);
+
+// f. add on desire location
+//  var arr = ["red" , "blue", "green" , "pink"];
+//  var userinput = +prompt("enter your position");
+//  var userinput1 = prompt("enter the colour");
+//  arr.splice(userinput, 0, userinput1);
+//  console.log(arr);
+
+//  g. delete  how many u want
+
+// var colors = ["red", "green", "blue", "yellow", "orange"];
+// console.log("Original Array: " + colors);
+
+// var index = prompt("Enter the index at which you want to delete color(s): ");
+// var count = prompt("Enter the number of colors you want to delete: ");
+
+// colors.splice(index, count);
+// console.log("Updated Array: " + colors);
+
+// Q10 ANSWER
+
+// var scores = [320,230,480,120];
+// console.log("Score of students/Original Scores: " + scores);
+// scores.sort();
+// console.log("Ordered score of student/Sorted Scores: " + scores);
+
+
+// Q11 ANSWER 
+
+// var cities = ["karachi", "Lahore", "Islamabad","Quetta","Peshawar"];
+// console.log("Cities list: " + cities);
+// var selectedCities = cities.slice(2,4);
+// console.log("Selected cities list: " + selectedCities);
+
+
+// Q12 ANSWER
+
+// var  arr =["This ", "is ", "my ", "cat"];
+// console.log("Array: "+ arr);
+// var str =arr.join("");
+// console.log("String: " + str);
+
+// Q13 ANSWER
+
+// var array =[];
+
+// array [0] = ("Keyboard");
+// array [1] =("Mouse");
+// array [2] =("Printer");
+// array [3] =("Monitor");
+// console.log(array);
+
+// console.log(array.shift());
+// console.log(array.shift());
+// console.log(array.shift());
+// console.log(array.shift());
+
+
+// another way /one 
+
+// var array =[];
+
+// array.push("Keyboard");
+// array.push("Mouse");
+// array.push("Printer");
+// array.push("Monitor");
+// console.log("Devices: "+ array);
+
+// console.log("Out: " + array.shift()); 
+// console.log("Out: " + array.shift()); 
+// console.log("Out: " + array.shift()); 
+// console.log("Out: " + array.shift()); 
+
+
+
+// Q14 ANSWER
+
+
+// var arr =[];
+
+// arr.push("Keyboard");
+// arr.push("Mouse");
+// arr.push("Printer");
+// arr.push("Monitor");
+
+//  console.log("Devices: "+ arr);
+
+//  console.log("Out: " +arr.pop());
+//  console.log("Out: " +arr.pop());
+//  console.log("Out: " +arr.pop());
+//  console.log("Out: " +arr.pop());
+
+
+// Q15 ANSWER not done /
+
+
